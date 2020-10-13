16c2d64c88cebf579c2bc5b21cf0da58fc91a803
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-10-13 13:06:38 -0700

    Add state inteface to App

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,32 @@ interface AppProps {
   color?: string
 }
 
-class App extends React.Component<AppProps> {
+interface AppState {
+  counter: number
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props)
+
+    this.state = { counter: 0 }
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 })
+  }
   render() {
-    return <div>Hi There {this.props.color}</div>
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        <div>{this.state.counter}</div>
+      </div>
+    )
   }
 }
 
